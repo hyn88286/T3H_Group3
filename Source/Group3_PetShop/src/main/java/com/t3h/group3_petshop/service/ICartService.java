ad87e957b5f74b47dd8bdbfde08ed3ea204f1f18
huy2862002 <nquanghuy337@gmail.com>
2024-03-22 23:53:28 +0700

    Api add to cart & show cart

---
+++ Source/Group3_PetShop/src/main/java/com/t3h/group3_petshop/service/ICartService.java
@@ -0,0 +1,13 @@
+package com.t3h.group3_petshop.service;
+
+import com.t3h.group3_petshop.model.dto.CartDTO;
+import com.t3h.group3_petshop.model.dto.ProductDTO;
+import com.t3h.group3_petshop.model.request.CartFilterRequest;
+import com.t3h.group3_petshop.model.request.ProductFilterRequest;
+import com.t3h.group3_petshop.model.response.BaseResponse;
+import org.springframework.data.domain.Page;
+
+public interface ICartService {
+    BaseResponse<Page<CartDTO>> getAll(CartFilterRequest filterRequest, int page, int size);
+    BaseResponse<?> addToCart(CartDTO cartDTO);
+}
