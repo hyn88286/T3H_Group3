5fb453e54ebd6b62d6f491490324f37b728b33fd
Cong <21111062@gmail.com>
2024-03-26 17:18:12 +0700

    dangky

--- Source/Group3_PetShop/src/main/java/com/t3h/group3_petshop/service/IUserService.java
+++ Source/Group3_PetShop/src/main/java/com/t3h/group3_petshop/service/IUserService.java
@@ -1,7 +1,10 @@
 package com.t3h.group3_petshop.service;
 
+import com.t3h.group3_petshop.entity.UserEntity;
 import com.t3h.group3_petshop.model.dto.UserDTO;
 public interface IUserService {
     UserDTO findUserByUsername(String username);
-     void addUser(UserEntity userEntity);
+    void saveUser(UserEntity user);
+    void updateUser(UserEntity user);
+    void deleteUser(String username);
 }
