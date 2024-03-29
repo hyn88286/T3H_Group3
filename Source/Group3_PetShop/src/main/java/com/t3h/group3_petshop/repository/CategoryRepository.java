71e163a8dcb7d69eb5bf192414b65b5b8599a8ff
Cong <21111062@gmail.com>
2024-03-29 09:54:11 +0700

    update cart

--- Source/Group3_PetShop/src/main/java/com/t3h/group3_petshop/repository/CategoryRepository.java
+++ Source/Group3_PetShop/src/main/java/com/t3h/group3_petshop/repository/CategoryRepository.java
@@ -1,11 +1,12 @@
 package com.t3h.group3_petshop.repository;
 
 import com.t3h.group3_petshop.entity.CategoryEntity;
+import com.t3h.group3_petshop.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<CategoryEntity,Long> {
-
+    CategoryEntity findByName(String name);
 
 }
