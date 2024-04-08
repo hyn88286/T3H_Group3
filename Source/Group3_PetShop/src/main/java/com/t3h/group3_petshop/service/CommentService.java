2cdcab9a5a09d4d4e2ad768d8a75aa1317560122
Cong <21111062@gmail.com>
2024-04-08 21:28:48 +0700

    update commen anh category

---
+++ Source/Group3_PetShop/src/main/java/com/t3h/group3_petshop/service/CommentService.java
@@ -0,0 +1,16 @@
+package com.t3h.group3_petshop.service;
+
+import com.t3h.group3_petshop.model.dto.CommentDTO;
+import com.t3h.group3_petshop.model.request.CommentFilterRequest;
+import com.t3h.group3_petshop.model.response.BaseResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface CommentService {
+    BaseResponse<Page<CommentDTO>> getAll(CommentFilterRequest commentFilterRequest, int page, int size);
+    BaseResponse<CommentDTO> addComment(CommentDTO commentDTO);
+    BaseResponse<CommentDTO> updateComment(Long commentId, CommentDTO commentDTO);
+
+    BaseResponse<?> deleteComment(Long commentId);
+}
