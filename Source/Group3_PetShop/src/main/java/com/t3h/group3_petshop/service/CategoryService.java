f42bf1f76f1edaeaebee34584482248aa2010627
PhucManh <ducxt47@gmail.com>
2024-05-05 00:56:38 +0700

    update code

--- Source/Group3_PetShop/src/main/java/com/t3h/group3_petshop/service/CategoryService.java
+++ Source/Group3_PetShop/src/main/java/com/t3h/group3_petshop/service/CategoryService.java
@@ -1,18 +1,14 @@
 package com.t3h.group3_petshop.service;
 
 import com.t3h.group3_petshop.entity.CategoryEntity;
-import com.t3h.group3_petshop.entity.UserEntity;
 import com.t3h.group3_petshop.model.dto.CategoryDTO;
-import com.t3h.group3_petshop.model.dto.OrderDTO;
 import com.t3h.group3_petshop.model.request.CategoryFilterRequest;
-import com.t3h.group3_petshop.model.request.OrderFilterRequest;
 import com.t3h.group3_petshop.model.response.BaseResponse;
 import org.springframework.data.domain.Page;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
 
-@Service
+
+
 public interface CategoryService {
 
     BaseResponse<Page<CategoryDTO>> getAll(CategoryFilterRequest filterRequest, int page, int size);
--- Source/Group3_PetShop/src/main/java/com/t3h/group3_petshop/service/CommentService.java
+++ Source/Group3_PetShop/src/main/java/com/t3h/group3_petshop/service/CommentService.java
@@ -4,9 +4,8 @@ import com.t3h.group3_petshop.model.dto.CommentDTO;
 import com.t3h.group3_petshop.model.request.CommentFilterRequest;
 import com.t3h.group3_petshop.model.response.BaseResponse;
 import org.springframework.data.domain.Page;
-import org.springframework.stereotype.Service;
 
-@Service
+
 public interface CommentService {
     BaseResponse<Page<CommentDTO>> getAll(CommentFilterRequest commentFilterRequest, int page, int size);
     BaseResponse<CommentDTO> addComment(CommentDTO commentDTO);
